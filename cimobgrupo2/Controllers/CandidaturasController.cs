f5cd72d77412edb0762a3896333c3482c6c3739e
costeira <33670371+costeira@users.noreply.github.com>
2018-01-08 16:18:52 +0000

    candidaturas continuacao

--- cimobgrupo2/Controllers/CandidaturasController.cs
+++ cimobgrupo2/Controllers/CandidaturasController.cs
@@ -8,18 +8,25 @@ using cimobgrupo2.Models;
 using cimobgrupo2.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Http;
 
 namespace cimobgrupo2.Controllers
 {
     public class CandidaturasController : Controller
     {
+        private string BASE_PATH = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", "candidaturas");
+
         private readonly List<Ajuda> _ajudas;
         private readonly List<Erro> _erros;
+        private FileController _fileController;
         private List<Candidatura> _candidaturas;
         private ApplicationDbContext _context;
 
-        public CandidaturasController(ApplicationDbContext context)
+        public CandidaturasController(ApplicationDbContext context, IFileProvider fileProvider)
         {
+            _fileController = new FileController(fileProvider);
             this._context = context;
             //vai buscar a lista de candidaturas
             _candidaturas = context.Candidaturas.Include(c => c.Curso).Include(c => c.Programa).Include(c => c.EscolaParceira).Include(c => c.User).ToList();
@@ -51,6 +58,10 @@ namespace cimobgrupo2.Controllers
                 Value = c.CursoId.ToString(),
                 Text = c.Nome
             }).ToList();
+
+            var caminho = "candidaturas/" + this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            ViewBag.Documentos = _fileController.GetFiles(caminho);
+
             return View(ProperView("Criar"));
         }
 
@@ -72,6 +83,38 @@ namespace cimobgrupo2.Controllers
         public IActionResult Detalhes(int? id)
         {
             return View(ProperView("Detalhes"), _candidaturas.Find(p => p.CandidaturaId == id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssociarDocumentos(List<IFormFile> files)
+        {
+            if (files.Count() > 0)
+            {
+                var caminho = Path.Combine(BASE_PATH, this.User.FindFirstValue(ClaimTypes.NameIdentifier));
+                foreach (IFormFile f in files)
+                {
+                    await _fileController.UploadFile(caminho, f);
+                }
+                SetSuccessMessage(files.Count() + " documentos adicionados.");
+            }
+
+            return RedirectToAction(nameof(Criar));
+        }
+
+        public IActionResult RemoverDocumentos(string[] ficheiros)
+        {
+            if (ficheiros.Count() > 0)
+            {
+                var caminho = Path.Combine(BASE_PATH, User.FindFirstValue(ClaimTypes.NameIdentifier));
+                foreach (string s in ficheiros)
+                {
+
+                    _fileController.Delete(caminho, s);
+
+                }
+                SetSuccessMessage(ficheiros.Count() + " documentos removidos.");
+            }
+            return RedirectToAction(nameof(Criar));
         }
 
         public String ProperView(String viewName)

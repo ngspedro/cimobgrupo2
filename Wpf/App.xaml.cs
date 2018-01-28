4625ca79366203328d4f42ee41e83ce1043b275f
costeira <150221103@estudantes.ips.pt>
2018-01-28 00:38:34 +0000

    inicio

---
+++ Wpf/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using cimobgrupo2.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        Curso c = new Curso() {
+            Nome = "asd"
+      };
+    }
+}
